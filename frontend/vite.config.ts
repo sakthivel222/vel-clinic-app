8c47323a9f881fa6641bfe9d2c6e76f73e6820ab
sakthivel222 <sakthivel222@github.com>
2026-09-04 11:02:12 +0530

    Fix PWA cache pattern

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -54,7 +54,7 @@ export default defineConfig({
             }
           },
           {
-            urlPattern: /^http:\/\/localhost:5000\/api\/.*/i,
+            urlPattern: /\/api\/.*/i,
             handler: 'NetworkFirst',
             options: {
               cacheName: 'api-cache',
